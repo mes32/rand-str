053ac565331bb5dfb36f70bc8fe3220c3b17e70d
mes32 <stockman.mike@gmail.com>
2022-12-24 18:56:47 -0600

    add a usage statement

--- src/main.c
+++ src/main.c
@@ -6,6 +6,20 @@
 const static int MY_SEED = 0; 
 // const static char LOWERCASE_LETTERS[] = "abc";
 
+void printUsage() {
+    printf("Usage: rand-str [-h] [-l length] [-c count] [-a] [-A] [-n] [-s] [-xa]\n");
+    printf("  Options:\n");
+    printf("    -h: Print usage screen\n");
+    printf("    -l length: Number of characters in random string (default 16)\n");
+    printf("    -c count: How many random strings to generate per run (default 1)\n");
+    printf("    -a: Include lowercase letters\n");
+    printf("    -A: Include uppercase letters\n");
+    printf("    -n: Include numbers\n");
+    printf("    -s: Include symbols\n");
+    printf("    -xa: Exclude visually ambiguous characters\n");
+    exit(0);
+}
+
 const char* randomString(size_t length) {
     char charset[] = "abc";
     size_t charsetSize = strlen(charset);
@@ -31,7 +45,10 @@ int main(int argc, char** argv) {
         printf("argv[%d] = %s\n", i, argv[i]);
     }
 
-    printf("randomeString = %s\n", randomString(10));
+    if (argv[1] && strcmp(argv[1], "-h") == 0) {
+        printUsage();
+    }
 
+    printf("%s\n", randomString(10));
     return 0;
 }
