36fce7bb6f6c1da39d82177e75a248b1a37751a7
mes32 <stockman.mike@gmail.com>
2023-02-02 01:44:49 -0600

    get working for base cases

--- src/main.c
+++ src/main.c
@@ -6,11 +6,11 @@
 #include <time.h>
 
 const static int MY_SEED = 0; 
-// TODO: Make these arrays complete
-// const static char LOWERCASE_LETTERS[] = "abc";
-// const static char UPPERCASE_LETTERS[] = "ABC";
-// const static char NUMBERS[] = "123";
-// const static char SYMBOLS[] = "@#$";
+// TODO: Make sure these arrays are complete
+const static char LOWERCASE_LETTERS[] = "abcdefghijklmnopqrstuvwxyz";
+const static char UPPERCASE_LETTERS[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const static char NUMBERS[] = "1234567890";
+const static char SYMBOLS[] = "!\"#$%&'()*+,-./:;<=>?@`\\~[]]^_{}";
 // const static char AMBIGUOUS_CHARACTERS[] = "o0O";
 
 int stringLength = 16;
@@ -44,6 +44,7 @@ bool isTruthy(char* str) {
     return strcmp("false", str); // || strcmp("0", str) || strcmp("off", str) || strcmp("null", str) || strcmp("nil", str);
 }
 
+// TODO: Handle case where setting one input false typically results in all flags being false and "Floating point exception: 8"
 void setIncludesToFalse() {
     if (!hasSetIncludes) {
         hasSetIncludes = true;
@@ -55,12 +56,64 @@ void setIncludesToFalse() {
     }
 }
 
-const char* randomString(size_t length) {
-    char charset[] = "abc";
-    size_t charsetSize = strlen(charset);
+char* composeCharSet() {
+    // TODO: Print the status of include flags for now
+    printf("includeLowercase: %s\n", includeLowercase == 0 ? "false" : "true");
+    printf("includeUppercase: %s\n", includeUppercase == 0 ? "false" : "true");
+    printf("includeNumbers  : %s\n", includeNumbers == 0 ? "false" : "true");
+    printf("includeSymbols  : %s\n", includeSymbols == 0 ? "false" : "true");
+    printf("includeAmbiguous: %s\n", includeAmbiguous == 0 ? "false" : "true");
+    printf("\n");
 
-    // TODO: only call srand once
-    srand(time(NULL) + MY_SEED);
+    size_t length = 0;
+    if (includeLowercase) {
+        length += strlen(LOWERCASE_LETTERS);
+    }
+    if (includeUppercase) {
+        length += strlen(UPPERCASE_LETTERS);
+    }
+    if (includeNumbers) {
+        length += strlen(NUMBERS);
+    }
+    if (includeSymbols) {
+        length += strlen(SYMBOLS);
+    }
+
+    char* charSet = malloc(sizeof(char) * (length + 1));
+    if (charSet == NULL) {
+        printf("Out of memory. Could not allocate memory for character set string.");
+        exit(1);
+    }
+
+    size_t j = 0;
+    if (includeLowercase) {
+        for (size_t i = 0; i < strlen(LOWERCASE_LETTERS); i++) {
+            charSet[j++] = LOWERCASE_LETTERS[i];
+        }
+    }
+    if (includeUppercase) {
+        for (size_t i = 0; i < strlen(UPPERCASE_LETTERS); i++) {
+            charSet[j++] = UPPERCASE_LETTERS[i];
+        }
+    }
+    if (includeNumbers) {
+        for (size_t i = 0; i < strlen(NUMBERS); i++) {
+            charSet[j++] = NUMBERS[i];
+        }
+    }
+    if (includeSymbols) {
+        for (size_t i = 0; i < strlen(SYMBOLS); i++) {
+            charSet[j++] = SYMBOLS[i];
+        }
+    }
+    charSet[length] = '\0'; // TODO: Check if there is any value in doing this
+
+    // TODO: if includeAmbiguous return here, otherwise remove ambiguous characters from a copy of charSet
+    return charSet;
+}
+
+const char* randomString(size_t length, char* charset) {
+    size_t charsetSize = strlen(charset);
 
     char* randomString = malloc(sizeof(char) * (length + 1));
     if (randomString == NULL) {
@@ -71,7 +124,7 @@ const char* randomString(size_t length) {
         int randomIndex = rand() % charsetSize;
         randomString[i] = charset[randomIndex];
     }
-    randomString[length] = '\0';
+    randomString[length] = '\0'; // TODO: Check if there is any value in doing this
 
     return randomString;
 }
@@ -156,16 +209,11 @@ int main(int argc, char** argv) {
         }
     }
 
-    // TODO: Print the status of include flags for now
-    printf("includeLowercase: %s\n", includeLowercase == 0 ? "false" : "true");
-    printf("includeUppercase: %s\n", includeUppercase == 0 ? "false" : "true");
-    printf("includeNumbers  : %s\n", includeNumbers == 0 ? "false" : "true");
-    printf("includeSymbols  : %s\n", includeSymbols == 0 ? "false" : "true");
-    printf("includeAmbiguous: %s\n", includeAmbiguous == 0 ? "false" : "true");
+    char* charSet = composeCharSet();
 
+    srand(time(NULL) + MY_SEED);
     for (int i = 0; i < numberOfStrings; i++) {
-        // TODO: Fix bug where numberOfStrings > 1 produces the same string
-        printf("%s\n", randomString(stringLength));
+        printf("%s\n", randomString(stringLength, charSet));
     }
     return 0;
 }
